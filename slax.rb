b6ef20d6488a7c148250a3e43b1e45d61664fb0c
Don Goodman-Wilson <don@slack-corp.com>
2017-01-21 15:59:12 -0800

    Wrong SHA, whoops

--- slax.rb
+++ slax.rb
@@ -2,7 +2,7 @@ class Slax < Formula
   desc "Slack Events API Explorer: A command line tool for inspecting the events Slack sends to your app"
   homepage "https://github.com/DEGoodmanWilson/slax"
   url "https://github.com/DEGoodmanWilson/slax/releases/download/v1.0/slax-mac-1.0.tar.gz"
-  sha256 "bf9bc3e316028b68a0b0370585ce2070f8671be858390b87501cfe48ff0a6f27"
+  sha256 "fd04e56f4e604ce2fc615ac3534303e074cc4b35e4bb15afbb60f31382ea3985"
 
   bottle :unneeded
 
