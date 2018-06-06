dc5c3a2d0d224c60478e4c53329be5d421aba2f8
Don Goodman-Wilson <don@goodman-wilson.com>
2018-06-06 09:06:43 +0200

    Update Pandia to 1.0.2

--- pandia.rb
+++ pandia.rb
@@ -1,8 +1,8 @@
 class Pandia < Formula
   desc "Just serves static files quickly, making front-end development easier."
   homepage "https://github.com/DEGoodmanWilson/pandia"
-  url "https://github.com/DEGoodmanWilson/pandia/releases/download/v1.0.1/pandia-mac-1.0.1.tar.gz"
-  sha256 "457498b587c67ac1b5851a5bb3526b08533c1f15414861b97e3598c959a4e362"
+  url "https://github.com/DEGoodmanWilson/pandia/releases/download/v1.0.2/pandia-mac-1.0.2.tar.gz"
+  sha256 "e779fbd773788b9e623191b5f9dc65a8cbf9f42a8c46f7d7b0ff5261fa1984a6"
 
   bottle :unneeded
 
