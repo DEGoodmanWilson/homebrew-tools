9d3fe5a29ff6bf2e0daca7812a61e0230cd1870f
Don Goodman-Wilson <degoodmanwilson@github.com>
2018-06-06 14:26:54 +0200

    Update pandia hash

--- pandia.rb
+++ pandia.rb
@@ -2,7 +2,7 @@ class Pandia < Formula
   desc "Just serves static files quickly, making front-end development easier."
   homepage "https://github.com/DEGoodmanWilson/pandia"
   url "https://github.com/DEGoodmanWilson/pandia/releases/download/v1.0.2/pandia-mac-1.0.2.tar.gz"
-  sha256 "e779fbd773788b9e623191b5f9dc65a8cbf9f42a8c46f7d7b0ff5261fa1984a6"
+  sha256 "c409c2af83448de893b614af68f45d0a1a56d9413308e35b4bce5044c4157f62"
 
   bottle :unneeded
 
