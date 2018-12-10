3b05ebf0f24d464f19c72ee9fc5eaf228c9ca5e7
Don Goodman-Wilson <degoodmanwilson@github.com>
2018-12-10 16:28:49 +0100

    Wrong version!

--- luna.rb
+++ luna.rb
@@ -1,7 +1,7 @@
 class Luna < Formula
   desc "Luna project boilerplate CLI."
   homepage "https://github.com/DEGoodmanWilson/luna-cli"
-  url "https://github.com/DEGoodmanWilson/luna-cli/releases/download/v1.1.0/luna-1.1.0.tar.gz"
+  url "https://github.com/DEGoodmanWilson/luna-cli/releases/download/v1.0.0/luna-1.0.0.tar.gz"
   sha256 "1c05ff03fdb9e34e2ae6baa596f1e87e414df6b587b05330bfd31c75ff365225"
 
   bottle :unneeded
