6ce22343355d01ef677df2e571694255289a6b3d
Don Goodman-Wilson <degoodmanwilson@github.com>
2018-12-13 09:51:26 +0100

    Bump luna version

--- luna.rb
+++ luna.rb
@@ -1,8 +1,8 @@
 class Luna < Formula
   desc "Luna project boilerplate CLI."
   homepage "https://github.com/DEGoodmanWilson/luna-cli"
-  url "https://github.com/DEGoodmanWilson/luna-cli/releases/download/v1.0.0/luna-1.0.0.tar.gz"
-  sha256 "1c05ff03fdb9e34e2ae6baa596f1e87e414df6b587b05330bfd31c75ff365225"
+  url "https://github.com/DEGoodmanWilson/luna-cli/releases/download/v1.0.1/luna-1.0.1.tar.gz"
+  sha256 "7989e48b1bb080594689293aaa643d875eda4cb176dc21c8661e8df923735dc7"
 
   bottle :unneeded
 
